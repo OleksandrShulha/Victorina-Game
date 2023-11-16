25108c685002ec47b54b6c0a05da1546153c5f78
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2023-11-16 12:09:51 +0200

    Choose Answer on Quiz

--- Quiz Game/Assets/Scripts/Quiz.cs
+++ Quiz Game/Assets/Scripts/Quiz.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -11,11 +12,41 @@ public class Quiz : MonoBehaviour
     [SerializeField] QustionSO qustionSO;
 
     //обєкти для виведення відповідей
-    //[SerializeField] GameObject[] answerButtons;
-
     [SerializeField] GameObject[] answerButtons;
 
+    //змінні для зберігання спрайтів по кілку
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correcAnswerSprite;
+
+
     void Start()
+    {
+        GetNextQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if(index == qustionSO.GetCorrectIndexAnswer())
+        {
+            questionText.text = "Corecr!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            answerButtons[index].GetComponent<Image>().color = Color.green;
+            buttonImage.sprite = correcAnswerSprite;
+
+            //можна і так:
+            // answerButtons[index].GetComponent<Image>().sprite = correcAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "Dont Corecr!";
+            answerButtons[index].GetComponent<Image>().color = Color.red;
+            answerButtons[qustionSO.GetCorrectIndexAnswer()].GetComponent<Image>().sprite = correcAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void DisplayQuestion()
     {
         //виводим в поле питання з обєкту
         questionText.text = qustionSO.GetQuestion();
@@ -24,7 +55,36 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = qustionSO.GetAnswer(i);
+
+            //можна щей так:
+            //  answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = qustionSO.GetAnswer(i);
         }
     }
+
+    void SetButtonState(bool stete)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Button>().interactable = stete;
+        }
+    }
+
+    void GetNextQuestion()
+    {
+        
+        SetButtonState(true);
+        SetDefaultButtonSprite();
+        DisplayQuestion();
+    }
+
+    void SetDefaultButtonSprite()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Image>().sprite = defaultAnswerSprite;
+            answerButtons[i].GetComponent<Image>().color = Color.white;
+        }
+    }
+
 
 }
