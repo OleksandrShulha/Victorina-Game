13a9b0cee31261ef46eb12af4e0b090af1174a97
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2023-11-22 18:36:32 +0200

    quiz control

--- Quiz Game/Assets/Scripts/Quiz.cs
+++ Quiz Game/Assets/Scripts/Quiz.cs
@@ -3,30 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     //поле для виведеня питання
     [SerializeField] TextMeshProUGUI questionText;
     //обєкт де ми берем питання
-    [SerializeField] QustionSO qustionSO;
+    QustionSO qustionCurent;
 
+    [SerializeField] List<QustionSO> qustion; 
+
+    [Header("Answers")]
     //обєкти для виведення відповідей
     [SerializeField] GameObject[] answerButtons;
-
     //змінні для зберігання спрайтів по кілку
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correcAnswerSprite;
+    bool hasAnswering;   //відповіли на питання чи ні
+
+    [Header("Timer")]
+    //для работы картинки таймера
+    [SerializeField] Image timerImage;
+    Timer timer;
 
 
     void Start()
     {
+        timer = FindAnyObjectByType<Timer>();
+
+
+        //виводим питання і відповіді
         GetNextQuestion();
     }
 
-    public void OnAnswerSelected(int index)
+    void Update()
     {
-        if(index == qustionSO.GetCorrectIndexAnswer())
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnswering = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnswering && !timer.isAnsweringQueastion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
+
+    }
+
+    void GetNextQuestion()
+    {
+
+        SetButtonState(true);  //включаеем всі кнопки
+        SetDefaultButtonSprite(); //на всі кнопкі вімашем спрайт по умолчанію
+        GetRendomQuestion();
+        DisplayQuestion(); //виводим питання
+    }
+
+    void GetRendomQuestion()
+    {
+        int index = UnityEngine.Random.Range(0, qustion.Count-1);
+        Debug.Log(index);
+        qustionCurent = qustion[index];
+    }
+
+    void DisplayAnswer(int index)
+    {
+        if (index == qustionCurent.GetCorrectIndexAnswer())
         {
             questionText.text = "Corecr!";
             Image buttonImage = answerButtons[index].GetComponent<Image>();
@@ -38,23 +85,37 @@ public class Quiz : MonoBehaviour
         }
         else
         {
-            questionText.text = "Dont Corecr!";
-            answerButtons[index].GetComponent<Image>().color = Color.red;
-            answerButtons[qustionSO.GetCorrectIndexAnswer()].GetComponent<Image>().sprite = correcAnswerSprite;
+            
+            if (index >=0)
+            {
+                questionText.text = "Dont Corecr!";
+                answerButtons[index].GetComponent<Image>().color = Color.red;
+            }
+            else
+            {
+                questionText.text = "Time is over";
+            }
+            answerButtons[qustionCurent.GetCorrectIndexAnswer()].GetComponent<Image>().sprite = correcAnswerSprite;
         }
+    }
 
+    public void OnAnswerSelected(int index)
+    {
+        hasAnswering = true;
+        DisplayAnswer(index);
         SetButtonState(false);
+        timer.CancelTimer();
     }
 
     void DisplayQuestion()
     {
         //виводим в поле питання з обєкту
-        questionText.text = qustionSO.GetQuestion();
+        questionText.text = qustionCurent.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = qustionSO.GetAnswer(i);
+            buttonText.text = qustionCurent.GetAnswer(i);
 
             //можна щей так:
             //  answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = qustionSO.GetAnswer(i);
@@ -69,13 +130,7 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    void GetNextQuestion()
-    {
-        
-        SetButtonState(true);
-        SetDefaultButtonSprite();
-        DisplayQuestion();
-    }
+
 
     void SetDefaultButtonSprite()
     {
--- Quiz Game/Assets/Scripts/Timer.cs
+++ Quiz Game/Assets/Scripts/Timer.cs
@@ -54,6 +54,5 @@ public class Timer : MonoBehaviour
                 loadNextQuestion = true;
             }
         }
-
     }
 }
